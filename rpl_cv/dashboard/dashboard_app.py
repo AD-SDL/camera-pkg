771b151bf8f97923dd50f23ca89459adf23a9b08
Amaan Khan <amaanite@gmail.com>
2022-07-07 18:12:51 -0500

    Add functionality for sending frames over socket

---
+++ rpl_cv/dashboard/dashboard_app.py
@@ -0,0 +1,156 @@
+import dash
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+import numpy as np
+from flask import Flask, Response
+import cv2
+import socket
+import struct
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)    # open webcam
+
+    def __del__(self):
+        self.video.release()
+
+    def _resizeImage(self, img, percent):
+        width = int(img.shape[1] * percent / 100)
+        height = int(img.shape[0] * percent / 100)
+        dim = (width, height)
+        img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+
+        return img, dim
+
+    def get_frame(self):
+        success, image = self.video.read()
+        scale_percent = 50 # percent of original size
+        image, _ = self._resizeImage(image, scale_percent)
+        image = cv2.flip(image, 1)   # flip horizontally
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+def read_bytes(sock, size):
+    buf_list = []
+    while sum(len(b) for b in buf_list) < size:
+        packet = sock.recv(4096)
+        if not packet:
+            raise ConnectionError
+        buf_list.append(packet)
+    return b''.join(buf_list)
+
+def poll_socket(ip='', port=8080):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((ip, port))
+    sock.listen()
+    print(f'Listening at ip:{ip} port:{port}')
+
+    while True:
+        sock_client, addr = sock.accept()
+        if not sock_client:
+            return
+        print(f'Got connection from: {addr}')
+
+        # Each message has a header containing the size of the message
+        # header_size is the size of the header itself
+        header_size = struct.calcsize('Q')
+        buffer = b''
+
+        try:
+            while True:
+                # Read the header
+                buffer += read_bytes(sock_client, header_size=len(buffer))
+                # Split the header from the front of the buffer, preserve the rest of the buffer
+                header, buffer = buffer[:header_size], buffer[header_size:]
+                # Extract the message size from the header
+                data_size = struct.unpack('Q', header)[0]
+
+                # Read the data
+                buffer += read_bytes(sock_client, data_size-len(buffer))
+
+                # Split the message from the front of the buffer, preserve the rest of the buffer
+                img_raw, buffer = buffer[:data_size], buffer[data_size:]
+                # # Decode the message into an image
+                # img = cv2.imdecode(np.frombuffer(img_raw, np.byte), cv2.IMREAD_ANYCOLOR)
+
+                # if callback_fn is not None:
+                #     callback_fn(img)
+                # TODO: check if you can simply yield img_raw --> decoding
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + img_raw + b'\r\n\r\n')
+
+
+        except ConnectionError:
+            print('Lost connection from:', addr)
+            # sock_client.shutdown(socket.SHUT_RDWR)
+            sock_client.close()
+
+
+
+
+server = Flask(__name__)
+app = dash.Dash(__name__, server=server)
+
+@server.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+app.layout = html.Div([
+    html.H2("Web Camera Output"),
+    html.Img(src="/video_feed", 
+                    style={
+                        'backgroundColor':'darkslategray',
+                        'color':'lightsteelblue',
+                        'height':'20%',
+                        'margin-left':'10px',
+                        'width':'20%',
+                        'text-align':'center',
+                        'display':'inline-block'
+                        }),
+
+    html.H2('Interactive normal distribution'),
+    dcc.Graph(id="graph", style={
+                        'backgroundColor':'darkslategray',
+                        'color':'lightsteelblue',
+                        'height':'35%',
+                        'margin-left':'10px',
+                        'text-align':'center',
+                        'width':'40%',
+                        'display':'inline-block'
+               }),
+
+    html.H4("Mean:"),
+    dcc.Slider(id="mean", min=-3, max=3, value=0, 
+               marks={-3: '-3', 3: '3'}),
+    html.H4("Standard Deviation:"),
+    dcc.Slider(id="std", min=1, max=3, value=1, 
+               marks={1: '1', 3: '3'}),
+])
+
+@app.callback(Output("graph", "figure"), Input("mean", "value"), Input("std", "value"))
+def display_color(mean, std):
+    data = np.random.normal(mean, std, size=500) # replace with your own data source
+    fig = px.histogram(data, range_x=[-10, 10])
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+'''
+All 3 files running on the NUC:
+    publisher.py  publishing the camera frames to a ros topic
+    subscriber.py fetching the camera frames from the ros topic and sending it on the socket server
+    dashboard.py  recieves images from the socket and displays on the web browser.
+
+longterm: join the subscriber.py with dashboard.py and remove socket
+
+'''
--- rpl_cv/dashboard/dashboard_ws2/src/py_pubsub/py_pubsub/subscriber_member_function.py
+++ rpl_cv/dashboard/dashboard_ws2/src/py_pubsub/py_pubsub/subscriber_member_function.py
@@ -22,42 +22,61 @@ from rclpy.node import Node # Handles the creation of nodes
 from sensor_msgs.msg import Image # Image is the message type
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 import cv2 # OpenCV library
+
+import socket
+import struct
  
 class CameraSubscriber(Node):
-  """
-  Create an CameraSubscriber class, which is a subclass of the Node class.
-  """
-  def __init__(self):
     """
-    Class constructor to set up the node
+    Create an CameraSubscriber class, which is a subclass of the Node class.
     """
-    # Initiate the Node class's constructor and give it a name
-    super().__init__('image_subscriber')
-      
-    self.subscription = self.create_subscription(
-      Image, 
-      'video_frames', 
-      self.listener_callback, 
-      10)
-    self.subscription # prevent unused variable warning
-      
-    # Used to convert between ROS and OpenCV images
-    self.br = CvBridge()
-    self.i = 0
-   
-  def listener_callback(self, data):
-    """
-    Callback function.
-    """
-    # Convert ROS Image message to OpenCV image
-    current_frame = self.br.imgmsg_to_cv2(data)
-    self.get_logger().info('Received video frame %d' % self.i)
-    # Display image
-    # cv2.imshow("camera", current_frame)
-    cv2.imwrite(f"Frame_{self.i}.jpg", current_frame)
-    self.i += 1
-    
-    cv2.waitKey(1)
+
+    def __init__(self, ip_conn='127.0.0.1', port=8080):
+        """
+        Class constructor to set up the node
+        """
+        # Initiate the Node class's constructor and give it a name
+        super().__init__('image_subscriber')
+            
+        self.subscription = self.create_subscription(
+            Image, 
+            'video_frames', 
+            self.listener_callback, 
+            10)
+        self.subscription # prevent unused variable warning
+            
+        # Used to convert between ROS and OpenCV images
+        self.br = CvBridge()
+        self.i = 0
+
+        self.sock = socket.create_connection((ip, port))
+
+    def listener_callback(self, data):
+        """
+        Callback function.
+        """
+        # Convert ROS Image message to OpenCV image
+        current_frame = self.br.imgmsg_to_cv2(data)
+        self.get_logger().info('Received video frame %d' % self.i)
+        # Display image
+        # cv2.imshow("camera", current_frame)
+        # cv2.imwrite(f"Frame_{self.i}.jpg", current_frame)
+        # cv2.waitKey(1)
+
+        # Send image over socket connection
+        self.send_frame(current_frame)
+
+        self.i += 1
+
+    def send_frame(self, img, quality=95):
+        _, img = cv2.imencode('.jpg', img, [cv2.IMWRITE_JPEG_QUALITY, quality])
+        data = img.tobytes()
+        header = struct.pack('Q', len(data))
+        self.sock.sendall(header + data)
+
+    def close_socket(self):
+        self.sock.close()
+
   
 def main(args=None):
   
